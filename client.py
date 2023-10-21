cbecb010483f1b8667117a62ee821a5268c4759b
Bhavya Giri <102273412+bhavya-giri@users.noreply.github.com>
2023-10-21 14:53:07 +0530

    feat: client progress

--- client.py
+++ client.py
@@ -23,3 +23,58 @@ def server(audio_data):
     result =response.json()
     return result["text"]
 
+def producer():
+    audio = pyaudio.PyAudio()
+    stream = audio.open(
+        format = pyaudio.paInt16,
+        channels = CHANNELS,
+        rate=RATE,
+        input = True,
+        frames_per_buffer=CHUNKS
+    )
+    
+    while True:
+        audio_data = b""
+        for _ in range(STEP_SEC):
+            chunk = stream.read(RATE)    
+            audio_data += chunk
+
+        audio_queue.put(audio_data)    
+
+def consumer(stats):
+    while True:
+        if length_queue.qsize() >= LENGTH_SEC:
+            with length_queue.mutex:
+                length_queue.queue.clear()
+                print()
+
+        audio_data = audio_queue.get()
+        transcription_start_time = time.time()
+        length_queue.put(audio_data)
+        audio_data_to_process = b""
+        for i in range(length_queue.qsize()):
+            audio_data_to_process += length_queue.queue[i]
+
+        try:
+            transcription = server(audio_data_to_process)
+            transcription = re.sub(r"\[.*\]", "", transcription)
+            transcription = re.sub(r"\(.*\)", "", transcription)
+        except:
+            transcription = "Error"
+
+        transcription_end_time = time.time()
+        transcription_to_visualize = transcription.ljust(MAX_CHARS, " ")
+
+        transcription_postprocessing_end_time = time.time()
+
+        sys.stdout.write('\033[K' + transcription_to_visualize + '\r')
+
+        audio_queue.task_done()
+
+        overall_elapsed_time = transcription_postprocessing_end_time - transcription_start_time
+        transcription_elapsed_time = transcription_end_time - transcription_start_time
+        postprocessing_elapsed_time = transcription_postprocessing_end_time - transcription_end_time
+        stats["overall"].append(overall_elapsed_time)
+        stats["transcription"].append(transcription_elapsed_time)
+        stats["postprocessing"].append(postprocessing_elapsed_time)
+    
